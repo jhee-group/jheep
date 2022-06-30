775eab406a572b7752cdaaf0e940b4c63a64c075
Jinserk Baik <823222+jinserk@users.noreply.github.com>
2022-06-30 14:39:14 -0400

    Minor change

--- backend/.settings/jupyter_server_config.py
+++ backend/.settings/jupyter_server_config.py
@@ -3,7 +3,7 @@
 import os
 from pathlib import Path
 
-JUPYTER_ROOT = os.environ.get("JUPYTER_ROOT", "/examples")
+JUPYTER_ROOT = os.environ.get("JHEEP_EXAMPLES_PATH", "/examples")
 
 #------------------------------------------------------------------------------
 # Application(SingletonConfigurable) configuration
